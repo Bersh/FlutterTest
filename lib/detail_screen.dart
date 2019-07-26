bc6a82160b033fc585017b92515d92c372eeb88b
Ilya Bershadskyi <ibersh20@gmail.com>
2019-07-26 15:05:22 +0200

    Added details page

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -3,67 +3,54 @@ import 'package:flutter/material.dart';
 import 'model/repo.dart';
 
 class DetailScreen extends StatelessWidget {
-  Repo _repo;
+  final Repo _repo;
 
   DetailScreen(this._repo);
 
   Widget _getDataText(String data) {
     return Expanded(
-//      alignment: Alignment.center,
-        child: Text(
-      data,
-      style: TextStyle(fontSize: 18),
-      maxLines: 3,
-      overflow: TextOverflow.ellipsis,
-    ));
+        child: Align(
+            alignment: Alignment.topLeft,
+            child: Text(
+              data,
+              style: TextStyle(fontSize: 20),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            )));
+  }
+
+  Widget _getTitleText(String title) {
+    return Text(title + ": ",
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold));
+  }
+
+  Widget _createDataRow(String title, String data) {
+    return Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [_getTitleText(title), _getDataText(data)],
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Repo details"),
-      ),
-      body: Container(
-          child: Align(
-        alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("ID: ",
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                _getDataText(_repo.id.toString())
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Name: ",
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                _getDataText(_repo.name)
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Full name: ",
-                    style:
-                        TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                _getDataText(_repo.fullName)
-              ],
-            ),
-          ],
+        appBar: AppBar(
+          title: Text("Repo details"),
         ),
-      )),
-    );
+        body: Container(
+            child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _createDataRow("ID", _repo.id.toString()),
+                _createDataRow("Name", _repo.name),
+                _createDataRow("Full name", _repo.fullName)
+              ]),
+        )));
   }
 }
